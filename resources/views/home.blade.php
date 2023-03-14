9fba819ae1bd4fac279c434cd36cca40f6deff31
Mobina <nimobina99@gmail.com>
2023-03-14 12:30:57 +0330

    update like & view section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="row justify-content-center">
         @for ($i = 1; $i < 10; $i++)
         <div class="col-md-6 mt-3 mb-3">
-            <div class="card">
+            <div class="card shadow-sm">
                 <img src="{{ asset('images/user-profile-avatar.jpg') }}" alt="avatar" class="img-thumbnail">
                 <div class="card-body">
-                    <p>
+                    <p class="font-weight-bolder">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci explicabo nesciunt natus velit minima voluptas suscipit ab, id cum nam esse! Molestias neque hic delectus quibusdam debitis dolor a animi.
                     </p>
                     <div class="row justify-content-center">
@@ -16,7 +16,12 @@
                             <span class="float-left"> publishe by nj</span>
                         </div>
                         <div class="col-md-6">
-                            <button class="float-right">Like</button>
+                            <div class="float-right">
+                                <span>200 <i class="fas fa-eye fa-1x"></i></span>
+                                <button class="bg-transparent border-0">
+                                    <i class="fas fa-heart fa-2x text-danger"></i>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
