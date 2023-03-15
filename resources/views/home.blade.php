084e9ce63ed1f763ed607c53856c0bf3fb7a390f
Mobina <nimobina99@gmail.com>
2023-03-15 09:58:07 +0330

    update home for submit_feed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,49 @@
 
 @section('content')
 <div class="container">
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-success btn-block shadow-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Create New Feed
+</button>
+
+<!-- Modal -->
+<form action="{{ route('submit_feed') }}" method="post" enctype="multipart/form-data">
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Create New Feed</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        @csrf
+        <div class="form-group">
+            <img src="" alt="">
+        </div>
+        <div class="form-group">
+            <label for="image">Image :</label>
+            <input type="file" class="form-control-file" name="image" id="image">
+        @error('image')
+            <div class="text-danger">
+                {{$message}}
+            </div>
+        @enderror
+        </div>
+        <div class="form-group">
+            <label for="description">Description :</label>
+            <textarea type="file" class="form-control" name="description" id="description" rows="10"></textarea>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+
     <div class="row justify-content-center">
         @for ($i = 1; $i < 10; $i++)
         <div class="col-md-6 mt-3 mb-3">
