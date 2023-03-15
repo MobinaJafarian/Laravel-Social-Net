8ebfac55f3da3b33ddb8b99a21c528ad8eaa38d8
Mobina <nimobina99@gmail.com>
2023-03-15 11:17:08 +0330

    update home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
         </div>
         <div class="form-group">
             <label for="image">Image :</label>
-            <input type="file" class="form-control-file" name="image" id="image">
+            <input type="file" class="form-control-file" name="image" id="image" accept="image/*">
         @error('image')
             <div class="text-danger">
                 {{$message}}
@@ -46,17 +46,17 @@
 </form>
 
     <div class="row justify-content-center">
-        @for ($i = 1; $i < 10; $i++)
+        @foreach ($feeds as $feed)
         <div class="col-md-6 mt-3 mb-3">
             <div class="card shadow-sm">
-                <img src="{{ asset('images/user-profile-avatar.jpg') }}" alt="avatar" class="img-thumbnail">
+                <img src="{{ $feed->image_url }}" alt="{{ $feed->description }}" class="img-thumbnail">
                 <div class="card-body">
                     <p class="font-weight-bolder">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci explicabo nesciunt natus velit minima voluptas suscipit ab, id cum nam esse! Molestias neque hic delectus quibusdam debitis dolor a animi.
+                        {{ $feed->description }}
                     </p>
                     <div class="row justify-content-center">
                         <div class="col-md-6">
-                            <span class="float-left"> publishe by nj</span>
+                            <span class="float-left"> published by {{ $feed->publisher->name}}</span>
                         </div>
                         <div class="col-md-6">
                             <div class="float-right">
@@ -70,7 +70,7 @@
                 </div>
             </div>
         </div>
-        @endfor
+        @endforeach
 
     </div>
 </div>
