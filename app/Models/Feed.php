eefcbabd01784441877a77c67b093de28aa99e9c
Mobina <nimobina99@gmail.com>
2023-03-15 11:18:43 +0330

    add relations

--- app/Models/Feed.php
+++ app/Models/Feed.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Feed extends Model
 {
     use HasFactory;
+
+    public function publisher()
+    {
+        return $this->belongsTo(User::class ,'publisher_id');
+    }
 }
