d158d5ca48966fd94dae1e6684e086d2874ee509
Mobina <nimobina99@gmail.com>
2023-03-15 09:57:51 +0330

    add submit_feed controller & model

---
+++ app/Http/Controllers/FeedController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Feed;
+use Illuminate\Http\Request;
+
+class FeedController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function submit_feed(Request $request)
+    {
+        $this->validate($request , [
+            'image' => 'required |max:10000',
+            // 'image' => 'required |max:10000',
+        ]);
+        $feed = new Feed();
+        $feed->image  = $request->file('image')->store('images');
+        $feed->description  = $request->description;
+        $feed->save() ? back() : "Error";
+        if($feed->save()){
+            return back();
+        }else{
+            return false;
+        }
+    
+    }
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Feed $feed)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Feed $feed)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Feed $feed)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Feed $feed)
+    {
+        //
+    }
+}
