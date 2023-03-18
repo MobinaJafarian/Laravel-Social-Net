4eb9ed8803344ef6db245740f0f67dde71154b66
Mobina <nimobina99@gmail.com>
2023-03-18 14:46:18 +0330

    add getLikesAttr

--- app/Models/Feed.php
+++ app/Models/Feed.php
@@ -9,8 +9,18 @@ class Feed extends Model
 {
     use HasFactory;
 
+    protected $appends = array('likes_count');
+
     public function publisher()
     {
         return $this->belongsTo(User::class ,'publisher_id');
+    }
+
+    public function getLikesCountAttribute()
+    {
+        return Like::where([
+            'feed_id' => $this->id,
+            'user_id' => auth()->user()->id,
+        ])->count();
     }
 }
