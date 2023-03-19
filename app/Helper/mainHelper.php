0bf4c3a58c2ff3b8c3c5933216b45db64ae55ec9
Mobina <nimobina99@gmail.com>
2023-03-19 22:38:29 +0330

    add check_user_if_followed

--- app/Helper/mainHelper.php
+++ app/Helper/mainHelper.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Follow;
 use App\Models\Like;
 
 function check_user_if_liked($feed , $user)
@@ -8,4 +9,12 @@ function check_user_if_liked($feed , $user)
             'feed_id' => $feed->id,
             'user_id' => $user
         ])->first()) ? false : true;
+    }
+    
+    function check_user_if_followed($follower , $following)
+    {
+        return empty(Follow::where([
+            'follower_id' => $follower,
+            'following_id' => $following
+        ])->first()) ? true : false;
     }
