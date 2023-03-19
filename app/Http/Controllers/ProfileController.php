726394bcff53ec9d8be742290830c7206b370707
Mobina <nimobina99@gmail.com>
2023-03-19 22:38:54 +0330

    add follow

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Feed;
+use App\Models\Follow;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -18,5 +19,22 @@ class ProfileController extends Controller
         $user = User::find($user);
         $feeds = Feed::where('publisher_id' , $user->id)->get();
         return view('profile' , compact('user' , 'feeds'));
+    }
+
+    public function follow($user)
+    {
+        if(check_user_if_followed(auth()->user()->id , $user)){
+           $follow = new Follow();
+           $follow->follower_id = auth()->user()->id;
+           $follow->following_id = $user;
+           $follow->save();
+           return back();
+        }else{
+            Follow::where([
+                'follower_id' => auth()->user()->id,
+                'following_id' => $user
+            ])->delete();
+            return back();
+        }
     }
 }
